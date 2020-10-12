9a7ea31365d3d024eaae44674f5764819b249a11
Jose Carlos Soto Morales <josecsmorales@gmail.com>
2020-10-11 21:43:07 -0300

    Adding maven wrapper.

--- src/main/java/com/jose/critter/repository/EmployeeRepository.java
+++ src/main/java/com/jose/critter/repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.jose.critter.repository;
 import com.jose.critter.entity.Employee;
 import com.jose.critter.util.EmployeeSkill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.DayOfWeek;
 import java.util.List;
